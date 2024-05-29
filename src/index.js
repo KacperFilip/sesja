5928b80c430aae8b29494300328ebd23cdc013ee
Kacper Filip <k.filip2004@gmail.com>
2024-05-29 19:11:20 +0200

    add analytics

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { NextUIProvider } from '@nextui-org/react';
+import { Analytics } from "@vercel/analytics/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <NextUIProvider>
   <main className='dark text-foreground bg-gradient-to-tr from-[#647909] to-[#0d5709]'>
     <App />
+    <Analytics/>
   </main>
   </NextUIProvider>
 );
